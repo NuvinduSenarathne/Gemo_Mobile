7420fab17cc7d1da37663fe399fde2785097269b
Muditha0829 <muditha1gms@gmail.com>
2023-08-27 14:10:16 +0530

    Cut Predicition :UI

--- lib/screens/cutPrediction.dart
+++ lib/screens/cutPrediction.dart
@@ -1,5 +1,9 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:gemo_app/constants/colors.dart';
+
+import 'cameraApp.dart';
+
 
 class CutPrediction extends StatelessWidget {
   const CutPrediction({super.key});
@@ -7,31 +11,37 @@ class CutPrediction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        backgroundColor: AppColors.backgroundColor,
-        title: const Text("Cut Prediction"),
-      ),
-      body: ListView(
-        children: const [
-          Card(
-            child: ListTile(
-              leading: Icon( Icons.camera_alt_outlined, size: 56.0),
-              title: Text('Camera'),
-              subtitle: Text('Capture photos with camera'),
-              trailing: Icon(Icons.navigate_next_outlined),
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: AppColors.backgroundColor,
+          title: const Text("Cut Prediction"),
+        ),
+        body: ListView(
+          children:  [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.camera_alt_outlined, size: 56.0),
+                title: Text('Camera'),
+                subtitle: Text('Capture photos with camera'),
+                trailing: Icon(Icons.navigate_next_outlined),
+                onTap: ()async {
+            await availableCameras().then((value) => Navigator.push(context,
+                MaterialPageRoute(builder: (_) => CameraApp(cameras: value))));
+          },
+              ),
             ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon( Icons.drive_file_move, size: 56.0),
-              title: Text('Gallery'),
-              subtitle: Text('Here is a second line'),
-              trailing: Icon(Icons.navigate_next_outlined),
-            ),
-          )
-        ],
-      )
-    );
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.drive_file_move, size: 56.0),
+                title: Text('Gallery'),
+                subtitle: Text('Here is a second line'),
+                trailing: Icon(Icons.navigate_next_outlined),
+                onTap: () {
+                  print('clicked files');
+                },
+              ),
+            )
+          ],
+        ));
   }
 }
