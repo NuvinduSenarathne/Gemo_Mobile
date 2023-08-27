c400ed65fd8e4788d60934ab5ea4a1cd3677dd26
Muditha0829 <muditha1gms@gmail.com>
2023-08-27 13:27:17 +0530

    Recommendation Result :UI

--- lib/screens/recommendationResult.dart
+++ lib/screens/recommendationResult.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../constants/colors.dart';
+import '../widgets/RecommendationTIle.dart';
 
 class RecommendationResult extends StatefulWidget {
   const RecommendationResult({super.key});
@@ -21,8 +22,9 @@ class _RecommendationResultState extends State<RecommendationResult> {
           title: const Text("Results"),
         ),
         body: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
-            child: Column(
+        padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
+        child: SingleChildScrollView(
+          child: Column(
               children: [
                 Container(
                   alignment: Alignment.centerLeft,
@@ -42,8 +44,7 @@ class _RecommendationResultState extends State<RecommendationResult> {
                         color: AppColors.formFieldTextColor,
                         fontSize: 20,
                         fontWeight: FontWeight.w400,
-                        decoration: TextDecoration.none
-                        ),
+                        decoration: TextDecoration.none),
                   ),
                 ),
                 Container(
@@ -67,24 +68,31 @@ class _RecommendationResultState extends State<RecommendationResult> {
                           decoration: TextDecoration.none),
                     )),
                 Container(
-                    margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    width: double.infinity,
-                    height: double.tryParse('50'),
-                    padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-                    child: Container(
-                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  child: Center(
-                    child: const Text(
-                    'Gemstone Recommendation',
-                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
-                  ),
-                  )
+                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  width: double.infinity,
+                  height: double.tryParse('50'),
+                  padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+                  child: Container(
+                      margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Center(
+                        child: const Text(
+                          'Gemstone Recommendation',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 24),
+                        ),
+                      )),
                 ),
-                    ),
-                    ListView(
-                      
-                    )
+                RecommendationTile(
+                  topic: "Ruby",
+                  image: "https://df2sm3urulav.cloudfront.net/tenants/gr/uploads/content/4wmnqlctgu0f3c9a.jpg",
+                  description: "Description about Ruby Description about Ruby Test Description about Ruby Description about Ruby TestDescription about Ruby Description about Ruby Test",
+                ),
+                RecommendationTile(
+                  topic: "Blue Sapphire",
+                  image: "https://punsirigems.com/cdn/shop/products/ILA4795_101_3000x3000.gif?v=1612151904",
+                  description: "Description about Blue Sapphire Description about Blue Sapphire",
+                ),
               ],
-            )));
+            ))));
   }
 }
---
+++ lib/widgets/RecommendationTIle.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class RecommendationTile extends StatelessWidget {
+  final String topic;
+  final String image;
+  final String description;
+
+  const RecommendationTile({
+    required this.topic,
+    required this.image,
+    required this.description,
+  }) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 150,
+            child: Image.network(image),
+          ),
+          const SizedBox(width: 10.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  topic,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20.0,
+                  ),
+                ),
+                Text(
+                  description,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
