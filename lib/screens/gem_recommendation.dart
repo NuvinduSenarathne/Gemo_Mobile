ae7b3cbd72e7678183ed87d347ced7b01dc8ca76
Kusalani Thennakoon <86671327+Kusalani@users.noreply.github.com>
2023-09-10 15:19:19 +0530

    Update gem_recommendation.dart

--- lib/screens/gem_recommendation.dart
+++ lib/screens/gem_recommendation.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gemo_app/screens/gem_recommendationResult.dart';
+import 'package:gemo_app/screens/gem_recommendationResults.dart';
 
 import '../constants/colors.dart';
 
