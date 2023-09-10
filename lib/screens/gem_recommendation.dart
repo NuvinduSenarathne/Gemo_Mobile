62bb7a617e170c0b421a28274907a1c5919767a9
Kusalani Thennakoon <kusalanithennakoon@192.168.1.3>
2023-09-10 13:34:37 +0530

    commit-recommendation

---
+++ lib/screens/gem_recommendation.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:gemo_app/screens/recommendationResult.dart';
+
+import '../constants/colors.dart';
+
+class Recommendation extends StatefulWidget {
+  const Recommendation({super.key});
+
+  @override
+  State<Recommendation> createState() => _RecommendationState();
+}
+
