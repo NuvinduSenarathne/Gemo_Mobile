6c61999af14fd755848b3794ae2e81a76a86188b
Kusalani Thennakoon <kusalanithennakoon@192.168.1.3>
2023-09-10 14:03:30 +0530

    commit_recommendation_result

---
+++ lib/screens/gem_recommendationResult.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../constants/colors.dart';
+import '../widgets/RecommendationTIle.dart';
+
+class RecommendationResult extends StatefulWidget {
+  const RecommendationResult({super.key});
+
+  @override
+  State<RecommendationResult> createState() => _RecommendationResultState();
+}
+
+class _RecommendationResultState extends State<RecommendationResult> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
