c60be365a2555ccfcbbdd803c7634827d55bbc20
Kusalani Thennakoon <kusalanithennakoon@192.168.1.3>
2023-09-10 13:39:50 +0530

    commit-recommendation-body

--- lib/screens/gem_recommendation.dart
+++ lib/screens/gem_recommendation.dart
@@ -15,3 +15,44 @@ class _RecommendationState extends State<Recommendation> {
     'Gemstone Information',
     'Recommend a Gemstone',
   ];
+
+  String selectedRequirement = 'Gemstone Information';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: AppColors.backgroundColor,
+          title: const Text("Recommendation"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: const Text(
+                    'Enter the your requirements',
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                  child: const Icon(
+                    Icons.light_mode,
+                    size: 40,
+                  ),
+                ),
+                
+                  
+              ],
+            )));
+  }
+}
